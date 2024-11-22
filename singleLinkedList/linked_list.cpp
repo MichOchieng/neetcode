ab1404ef336fc14d8e666b54e1710cfae0e3371c
Mich Ochieng (CNC) <michochieng@gmail.com>
2024-11-22 12:09:39 -0800

    First commit

---
+++ singleLinkedList/linked_list.cpp
@@ -0,0 +1,26 @@
+class LinkedList {
+public:
+    LinkedList() {
+
+    }
+
+    int get(int index) {
+
+    }
+
+    void insertHead(int val) {
+        
+    }
+    
+    void insertTail(int val) {
+
+    }
+
+    bool remove(int index) {
+        
+    }
+
+    vector<int> getValues() {
+        
+    }
+};
