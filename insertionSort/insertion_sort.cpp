4b6866e7f4140aabcdb997dc02ba5c0aae9cd96d
Mich Ochieng <michochieng@gmail.com>
2024-12-02 01:33:20 -0800

    First commit:

---
+++ insertionSort/insertion_sort.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <string>
+
+// Definition for a Pair
+class Pair
+{
+public:
+    int key;
+    std::string value;
+
+    Pair(int key, std::string value) : key(key), value(value) {}
+};
+
+class Solution
+{
+public:
+    std::vector<std::vector<Pair>> insertionSort(std::vector<Pair> &pairs)
+    {
+    }
+};
