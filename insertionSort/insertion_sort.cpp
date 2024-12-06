879053df5e6cdc58ecb78156a7f6059129f848a4
Mich Ochieng <michochieng@gmail.com>
2024-12-05 20:08:35 -0800

    Solution found

--- insertionSort/insertion_sort.cpp
+++ insertionSort/insertion_sort.cpp
@@ -16,5 +16,29 @@ class Solution
 public:
     std::vector<std::vector<Pair>> insertionSort(std::vector<Pair> &pairs)
     {
+        if (pairs.size() < 1)
+        {
+            return {};
+        }
+
+        std::vector<std::vector<Pair>> steps;
+        steps.push_back(pairs);
+
+        for (size_t i = 1; i < pairs.size(); i++)
+        {
+            int j = i - 1;
+            Pair temp = pairs[i]; // get key value for sorting
+
+            while (temp.key < pairs[j].key && j >= 0) // while the key is less than previous keys in range
+            {
+                pairs[j + 1] = pairs[j]; // swap pairs
+                j--;
+            }
+            pairs[j + 1] = temp;
+
+            steps.push_back(pairs);
+        }
+
+        return steps;
     }
 };
